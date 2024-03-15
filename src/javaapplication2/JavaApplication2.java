c0f6ffa100b83fd7adb10daf774ef6bd336f38eb
Luan Flavio Barros Menezes <110263423+Luan-FlavioBr@users.noreply.github.com>
2024-03-15 10:58:50 -0300

    [feat] ex01 e ex02

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class JavaApplication2 {
     public static void main(String[] args) {
-        ex02();
+        //JOptionPane.show
      }
     
     public static void ex01(){
