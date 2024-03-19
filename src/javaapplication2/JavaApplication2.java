c774578f5da2fae8fb61d1b0689db2f9c901e25d
Luan Flavio Barros Menezes <110263423+Luan-FlavioBr@users.noreply.github.com>
2024-03-19 08:43:42 -0300

    ex 03

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class JavaApplication2 {
     public static void main(String[] args) {
         //JOptionPane.show
+        ex04();
      }
     
     public static void ex01(){
@@ -31,7 +32,47 @@ public class JavaApplication2 {
         } else if(salario <= 7786.02){
             desconto = 0.15 * salario;
             JOptionPane.showMessageDialog(null, desconto);     
-        }
+        }      
+    }
+    
+    public static void ex03(){
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        float salario = Float.parseFloat(JOptionPane.showInputDialog("Informe seu salário: R$"));
+        double desconto = 0;
+        String mensagem;
         
+        if(salario <= 2259.20){
+            desconto = 0;
+            mensagem = String.format("O desconto do INSS do funcionário %s\n"
+                    + "foi de R$%.2f", nome, desconto);
+            JOptionPane.showMessageDialog(null, mensagem);     
+        } else if(salario > 2259.20 && salario <= 2826.65){
+            desconto = 0.075 * salario;
+            mensagem = String.format("O desconto do INSS do funcionário %s\n"
+                    + "foi de R$%.2f", nome, desconto);
+            JOptionPane.showMessageDialog(null, mensagem);     
+        } else if(salario <= 3751.05){
+            desconto = 0.15 * salario;
+            mensagem = String.format("O desconto do INSS do funcionário %s\n"
+                    + "foi de R$%.2f", nome, desconto);
+            JOptionPane.showMessageDialog(null, mensagem);     
+        } else if(salario <= 4664.68){
+            desconto = 0.2255 * salario;
+            mensagem = String.format("O desconto do INSS do funcionário %s\n"
+                    + "foi de R$%.2f", nome, desconto);
+            JOptionPane.showMessageDialog(null, mensagem);     
+        } else {
+            desconto = 0.275 * salario;
+            mensagem = String.format("O desconto do INSS do funcionário %s\n"
+                    + "foi de R$%.2f", nome, desconto);
+            JOptionPane.showMessageDialog(null, mensagem);     
+        }     
+    }
+    
+    public static void ex04(){
+        int diasInformados = Integer.parseInt(JOptionPane.showInputDialog("Informe seu salário: R$")); 
+        int anos, meses, dias;
+        
+        System.out.println(diasInformados / 365.0);
     }
 }
